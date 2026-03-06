c601dd1977bd008ee1369dde1499aac4476e8896
Alejandro Gómez <algono@inf.upv.es>
2026-03-06 09:05:00 +0100

    Small fix: defaultLocale should be of type Locale

--- src/languages.ts
+++ src/languages.ts
@@ -1,4 +1,4 @@
 // Define your languages here as a single source of truth
 export const locales = ['en', 'es', 'de', 'pt', 'it', 'hr'] as const;
-export const defaultLocale = 'en';
 export type Locale = typeof locales[number];
+export const defaultLocale: Locale = 'en';
