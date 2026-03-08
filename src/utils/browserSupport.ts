cbb80a9d36ab4a5ac7dfdbb952828891d7ca7f68
Alejandro Gómez <algono@inf.upv.es>
2026-03-08 22:09:15 +0100

    FIX better supportsCssAnchorPositioning function

--- src/utils/browserSupport.ts
+++ src/utils/browserSupport.ts
@@ -1,3 +1,3 @@
-export function supportsCssAnchorPositioning(document: Document) {
-  return "anchorName" in document.documentElement.style;
+export function supportsCssAnchorPositioning() {
+  return window.CSS && CSS.supports && CSS.supports('top', 'anchor(bottom)');
 }
