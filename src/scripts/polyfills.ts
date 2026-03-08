cfa21d1668b3db484210e3646193b4b912ff52e3
Alejandro Gómez <algono@inf.upv.es>
2026-03-08 22:46:04 +0100

    FIX don't dynamic import polyfill CSS inside dynamically imported JS

--- src/scripts/polyfills.ts
+++ src/scripts/polyfills.ts
@@ -2,10 +2,7 @@
  * Polyfill for dropdowns that don't support CSS anchor positioning.
  * Instead of relying on the popover API, we use JavaScript to toggle the visibility of the dropdown menu and manage ARIA attributes for accessibility.
  */
-export async function dropdownPolyfill(mediaQuery?: MediaQueryList) {
-  // Dynamically load the CSS for the dropdown polyfill. This way we only load it when necessary, avoiding unnecessary CSS for modern browsers.
-  await import('@styles/polyfills/polyfill-dropdown.css');
-
+export function dropdownPolyfill(mediaQuery?: MediaQueryList) {
   // Setup dropdowns on the page
   document.querySelectorAll('.dropdown-wrapper').forEach(wrapper => {
     const trigger = wrapper.querySelector<HTMLButtonElement>('.dropdown-trigger');
--- src/utils/browserSupport.ts
+++ src/utils/browserSupport.ts
@@ -1,3 +1,14 @@
-export function supportsCssAnchorPositioning() {
+export function supportsCssAnchorPositioning() : boolean {
   return false;//window.CSS && CSS.supports && CSS.supports('top', 'anchor(bottom)');
+}
+
+export const needsDropdownPolyfill : () => boolean = () => !supportsCssAnchorPositioning();
+/**
+ * Dynamically loads the dropdown polyfill for browsers that don't support CSS anchor positioning. This includes both the necessary CSS and JS to ensure the dropdowns work correctly, while avoiding loading unnecessary code for modern browsers that do support the feature.
+ */
+export async function loadDropdownPolyfill(mediaQuery?: MediaQueryList) {
+  // Dynamic import so we only load the polyfill when necessary, avoiding unnecessary CSS and JS for modern browsers.
+  await import('@styles/polyfills/polyfill-dropdown.css');
+  const { dropdownPolyfill } = await import('@scripts/polyfills.ts');
+  dropdownPolyfill(mediaQuery);
 }
