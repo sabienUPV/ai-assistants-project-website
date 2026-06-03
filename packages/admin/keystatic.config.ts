9f603ddb41d7d5aaba977ae796fc71b03040a4cd
Alejandro Gómez <algono@inf.upv.es>
2026-06-03 17:28:32 +0200

    FIX Keystatic image upload handle filenames and use required sharp library for image optimization

--- packages/admin/keystatic.config.ts
+++ packages/admin/keystatic.config.ts
@@ -18,6 +18,26 @@ export default config({
             image: {
               directory: 'src/assets/images/posts',
               publicPath: '@assets/images/posts/',
+              // Muy útil para arreglar nombres de archivos con tildes, espacios, mayúsculas, etc. que pueden dar problemas al subirlos a la web
+              transformFilename: (filename) => {
+                // Separamos el nombre de la extensión
+                const parts = filename.split('.');
+                const ext = parts.pop();
+                const name = parts.join('.');
+
+                // Aplicamos un slugify básico usando regex nativo de JS
+                const cleanName = name
+                  .toLowerCase()
+                  .normalize('NFD')                     // Descompone caracteres con tildes
+                  .replace(/[\u0300-\u036f]/g, '')      // Elimina los acentos
+                  .replace(/[^a-z0-9\s-]/g, '')         // Elimina caracteres raros
+                  .trim()
+                  .replace(/\s+/g, '-')                 // Cambia espacios por guiones
+                  .replace(/-+/g, '-');                 // Evita guiones dobles
+
+                // Devolvemos el nombre limpio con su extensión original
+                return `${cleanName}.${ext}`;
+              }
             },
           },
         }),
