2283638a7c921b9a4725509b5c5bd1bc3a071bd2
Alejandro Gómez <algono@inf.upv.es>
2026-06-03 17:19:23 +0200

    Keystatic - Use assets alias from tsconfig in links

--- packages/admin/keystatic.config.ts
+++ packages/admin/keystatic.config.ts
@@ -17,7 +17,7 @@ export default config({
           options: {
             image: {
               directory: 'src/assets/images/posts',
-              publicPath: '../../assets/images/posts/',
+              publicPath: '@assets/images/posts/',
             },
           },
         }),
