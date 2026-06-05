bae02f1d8bdffb58e766d4cab4c1459b54555037
Alejandro Gómez <algono@inf.upv.es>
2026-06-05 13:46:15 +0200

    FIX posts schema make most fields optional

--- packages/core/src/schemas/posts.ts
+++ packages/core/src/schemas/posts.ts
@@ -2,9 +2,9 @@ import { z } from 'astro/zod';
 
 export const postSchema = z.object({
   title: z.string(),
-  pubDate: z.date(),
-  description: z.string(),
-  author: z.string(),
+  pubDate: z.date().optional(),
+  description: z.string().optional(),
+  author: z.string().optional(),
 });
 
 export type Post = z.infer<typeof postSchema>;
