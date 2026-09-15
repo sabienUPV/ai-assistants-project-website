fd399ed15bd33be1b3b64d8f4b5e941cd7aeb557
Alejandro Gómez <algono@inf.upv.es>
2026-09-15 09:38:48 +0200

    fix(wordpress): Allow robots.txt requests even if frontend is meant to be non-indexable

--- wordpress/snippets/disable-wordpress-access-to-non-logged-users.php
+++ wordpress/snippets/disable-wordpress-access-to-non-logged-users.php
@@ -1,4 +1,24 @@
 <?php
+add_action( 'template_redirect', 'blank_404_if_logged_out' );
+/**
+ * Serves an empty HTTP 404 response (zero HTML/content) to logged-out frontend visitors.
+ * Exception: Allows robots.txt to be served properly for SEO crawlers.
+ * Note: Remember to check the "Ask Search Engines Not to Index This Site" option in Settings > Reading if you want to block search engines from indexing your site.
+ */
+function blank_404_if_logged_out() {
+    // If the request is for robots.txt, let WordPress generate it and abort this filter
+    // Note: Remember to check the "Ask Search Engines Not to Index This Site" option in Settings > Reading if you want to block search engines from indexing your site.
+    if ( is_robots() ) {
+        return;
+    }
+
+    if ( ! is_user_logged_in() ) {
+        status_header( 404 );
+        nocache_headers();
+        exit;
+    }
+}
+
 // Remove WordPress REST API and Shortlink headers from HTTP response
 add_filter( 'template_redirect', function() {
     if ( ! is_user_logged_in() ) {
@@ -6,18 +26,6 @@ add_filter( 'template_redirect', function() {
         remove_action( 'template_redirect', 'rest_output_link_header', 11 );
     }
 }, 9 );
-
-add_action( 'template_redirect', 'blank_404_if_logged_out' );
-/**
- * Serves an empty HTTP 404 response (zero HTML/content) to logged-out frontend visitors.
- */
-function blank_404_if_logged_out() {
-    if ( ! is_user_logged_in() ) {
-        status_header( 404 );
-        nocache_headers();
-        exit;
-    }
-}
 
 add_filter( 'rest_authentication_errors', 'restrict_rest_api_to_logged_in_users' );
 /**
